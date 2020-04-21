be238e5a5850b2ec2be3718f02c29a95537bf496
David Meyer <david@meyertime.com>
2020-04-21 10:52:44 -0500

    Start tracking davidrandr

---
+++ src/davidrandr.sh
@@ -0,0 +1,191 @@
+#!/bin/bash
+#set -e
+
+#echo "davidrandr $@" >>/tmp/davidrandr.log
+#env >>/tmp/davidrandr.log
+
+#echo "jakdajbirojekwlfnedkwanvdksapbnjiroe"
+
+LAPTOP=eDP-1-1
+THUNDERBOLT=DP-4
+DISPLAYPORT=DP-3
+HDMI=DP-1
+
+function nvidiaDisableAllTransforms()
+{
+    # Prevents RRSetScreenSize error later on
+    xrandr \
+        --output $THUNDERBOLT --transform none \
+        --output $DISPLAYPORT --transform none \
+        --output $HDMI --transform none \
+        --output $LAPTOP --transform none || true
+}
+
+function nvidiaUHDMobileProfile()
+{
+    #nvidiaDisableAllTransforms
+
+    #xrandr --fb 3840x2160 \
+    xrandr \
+        --output $THUNDERBOLT --off \
+        --output $DISPLAYPORT --off \
+        --output $HDMI --off \
+        --output $LAPTOP --mode 3840x2160 --pos 0x0 --primary
+}
+
+function nvidiaFHDMobileProfile()
+{
+    #nvidiaDisableAllTransforms
+
+    for i in {1..3}; do
+        xrandr \
+            --output $THUNDERBOLT --off \
+            --output $DISPLAYPORT --off \
+            --output $HDMI --off \
+            --output $LAPTOP --mode 1920x1080 --pos 0x0 --primary \
+            && break || sleep 5
+    done
+}
+
+function nvidiaUHDWorkProfile()
+{
+    #nvidiaDisableAllTransforms
+
+    # 4/3 scaling, combined with 200% global DPI equals 150%
+    #xrandr --fb 10240x5040 \
+    #    --output $THUNDERBOLT --mode 3840x2160 --scale-from 5120x2880 --pos 0x0 --panning 5120x2880+0+0 \
+    #    --output $DISPLAYPORT --mode 3840x2160 --scale-from 5120x2880 --pos 5120x0 --panning 5120x2880+5120+0 --primary \
+    #    --output $LAPTOP --mode 3840x2160 --transform none --pos 3200x2880 || true
+    #xrandr --fb 10240x5040 \
+    #    --output $THUNDERBOLT --mode 3840x2160 --scale-from 5120x2880 --pos 0x0 \
+    #    --output $DISPLAYPORT --mode 3840x2160 --scale-from 5120x2880 --pos 5120x0 --primary \
+    #    --output $LAPTOP --mode 3840x2160 --transform none --pos 3200x2880 || true
+
+    # 3/4 scaling of laptop monitor (2880x1620)
+    #xrandr --fb 7680x3780 \
+    #xrandr \
+    #    --output $THUNDERBOLT --mode 3840x2160 --pos 0x0 \
+    #    --output $DISPLAYPORT --mode 3840x2160 --pos 3840x0 --primary \
+    #    --output $LAPTOP --mode 2880x1620 --pos 2400x2160 || true
+
+    for i in {1..3}; do
+        xrandr \
+            --output $THUNDERBOLT --mode 3840x2160 --pos 0x0 \
+            --output $LAPTOP --off \
+            --output $DISPLAYPORT --off \
+            --output $HDMI --off \
+            && break || sleep 5
+    done
+    sleep 5
+    for i in {1..3}; do
+        xrandr \
+            --output $DISPLAYPORT --mode 3840x2160 --pos 3840x0 --primary \
+            && break || sleep 5
+    done
+    sleep 5
+    for i in {1..3}; do
+        xrandr \
+            --output $LAPTOP --mode 2880x1620 --pos 2400x2160 \
+            && break || sleep 5
+    done
+
+    #kquitapp5 plasmashell >/dev/null 2>&1
+    #sleep 3
+    #kstart5 plasmashell >/dev/null 2>&1
+}
+
+function nvidiaWorkNotScaledProfile()
+{
+    xrandr \
+        --output $THUNDERBOLT --mode 3840x2160 --transform none --pos 0x0 \
+        --output $DISPLAYPORT --mode 3840x2160 --transform none --pos 3840x0 --primary \
+        --output $LAPTOP --mode 3840x2160 --transform none --pos 1920x2160 \
+        --output $HDMI --off
+}
+
+function nvidiaHomeNotScaledProfile()
+{
+    xrandr \
+        --output $DISPLAYPORT --mode 1920x1200 --transform none --pos 0x0 \
+        --output $HDMI --mode 1920x1200 --transform none --pos 1920x0 --primary \
+        --output $LAPTOP --mode 3840x2160 --transform none --pos 0x1200 \
+        --output $THUNDERBOLT --off
+}
+
+function nvidiaHomeProfile()
+{
+    xrandr \
+        --output $DISPLAYPORT --mode 1920x1200 --scale-from 3840x2400 --pos 0x0 \
+        --output $HDMI --mode 1920x1200 --scale-from 3840x2400 --pos 3840x0 --primary \
+        --output $LAPTOP --mode 2880x1620 --transform none --pos 2400x2400 \
+        --output $THUNDERBOLT --off
+}
+
+function nvidiaFHDHomeProfile()
+{
+    for i in {1..3}; do
+        xrandr \
+            --output $DISPLAYPORT --mode 1920x1200 --pos 0x0 \
+            --output $HDMI --mode 1920x1200 --pos 1920x0 --primary \
+            --output $LAPTOP --mode 1440x810 --rate 60 --transform none --pos 1200x1200 \
+            --output $THUNDERBOLT --off \
+            && break || sleep 5
+    done
+
+    #for i in {1..3}; do
+    #    xrandr \
+    #        --output $DISPLAYPORT --mode 1920x1200 --pos 0x0 \
+    #        --output $LAPTOP --off \
+    #        --output $HDMI --off \
+    #        --output $THUNDERBOLT --off \
+    #        && break || sleep 5
+    #done
+    #sleep 5
+    #for i in {1..3}; do
+    #    xrandr \
+    #        --output $HDMI --mode 1920x1200 --pos 1920x0 --primary \
+    #        && break || sleep 5
+    #done
+    #sleep 5
+    #for i in {1..3}; do
+    #    xrandr \
+    #        --output $LAPTOP --mode 1440x810 --rate 60 --pos 1200x1200 \
+    #        && break || sleep 5
+    #done
+}
+
+PROFILE=$1
+
+if [ "$PROFILE" == "" ]; then
+    PROFILE=auto
+fi
+
+if [ "$PROFILE" == "auto" ]; then
+    PROFILE=
+    MONITORS=$(xrandr --query | grep '\bconnected\b' | awk '{print $1}')
+    NL=$'\n'
+    if [ "$MONITORS" == "$LAPTOP" ]; then PROFILE=mobile; fi
+    if [ "$MONITORS" == "$HDMI$NL$DISPLAYPORT$NL$LAPTOP" ]; then PROFILE=home; fi
+
+    if [ "$PROFILE" == "" ]; then
+        echo "Could not detect profile!"
+    else
+        echo "Detected profile: $PROFILE"
+    fi
+fi
+
+case $PROFILE in
+
+    mobile)
+        nvidiaFHDMobileProfile
+        ;;
+
+    work)
+        echo "Not supported yet for FHD!"
+        ;;
+
+    home)
+        nvidiaFHDHomeProfile
+        ;;
+
+esac

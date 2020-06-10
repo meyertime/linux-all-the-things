4731a3f68ca4650633ab45d3ab94e46f11af9864
David Meyer <david@meyertime.com>
2020-06-10 16:56:53 -0500

    More PulseAudio stuff

---
+++ src/pa-lt.sh
@@ -0,0 +1,181 @@
+#!/bin/bash
+set -e
+
+HEADSET_MIC=alsa_input.usb-C-Media_Electronics_Inc._USB_Audio_Device-00.mono-fallback
+VOICE_SOURCE=voice_final.monitor
+
+if [ "$SOURCE" == "" ]; then
+    pactl list short sources \
+        | grep $VOICE_SOURCE >/dev/null \
+        && SOURCE=$VOICE_SOURCE \
+        || SOURCE=$HEADSET_MIC
+fi
+
+HEADSET_OUT=alsa_output.usb-C-Media_Electronics_Inc._USB_Audio_Device-00.analog-stereo
+WEBRTC_SINK=voice_webrtc
+
+if [ "$SINK" == "" ]; then
+    pactl list short sinks \
+        | grep $WEBRTC_SINK >/dev/null \
+        && SINK=$WEBRTC_SINK \
+        || SINK=$HEADSET_OUT
+fi
+
+CLOCK_SINK=lt_clock
+HOST_SINK=lt_host
+RECORDER_SINK=lt_record
+SILENCE_SINK=lt_silence
+
+function setup {
+    pactl load-module module-null-sink \
+        sink_name=$CLOCK_SINK \
+        sink_properties='device.description="LT\ Clock"'
+
+    pactl load-module module-null-sink \
+        sink_name=$HOST_SINK \
+        sink_properties='device.description="LT\ Host"'
+
+    pactl load-module module-null-sink \
+        sink_name=$RECORDER_SINK \
+        sink_properties='device.description="LT\ Recorder"'
+
+    pactl load-module module-null-sink \
+        sink_name=$SILENCE_SINK \
+        sink_properties='device.description="LT\ Silence"'
+
+    pactl set-sink-mute $SILENCE_SINK 1
+
+    pactl load-module module-loopback \
+        source=$SOURCE \
+        sink=$HOST_SINK \
+        source_dont_move=true \
+        sink_dont_move=true \
+        latency_msec=1
+
+    pactl load-module module-loopback \
+        source=$CLOCK_SINK.monitor \
+        sink=$HOST_SINK \
+        source_dont_move=true \
+        sink_dont_move=true
+
+    pactl load-module module-loopback \
+        source=$CLOCK_SINK.monitor \
+        sink=$SINK \
+        source_dont_move=true \
+        sink_dont_move=true
+}
+
+function cleanup {
+    MODULES=$(pactl list short modules | grep -E '^[0-9]+\s+module-null-sink\s+sink_name=lt_' | sed -E 's/^([0-9]+).*$/\1/')
+
+    while read MODULE; do
+        if [ "$MODULE" != "" ]; then
+            pactl unload-module $MODULE
+        fi
+    done <<<"$MODULES"
+}
+
+function defaults {
+    pactl set-default-source $SOURCE
+    pactl set-default-sink $SINK
+}
+
+function move-apps {
+    OBS_SOURCE_OUTPUT=$(find-app-source-output OBS)
+    pactl move-source-output $OBS_SOURCE_OUTPUT $RECORDER_SINK.monitor
+
+    #FIREFOX_SOURCE_OUTPUT=$(find-app-source-output Firefox 2)
+    FIREFOX_SINK_INPUT=$(find-app-sink-input Firefox 3)
+    
+    # Assume the first Firefox is the host hangout
+    #HOST_SOURCE_OUTPUT=$(sed -n '1p' <<<"$FIREFOX_SOURCE_OUTPUT")
+    HOST_SINK_INPUT=$(sed -n '1p' <<<"$FIREFOX_SINK_INPUT")
+
+    # Assume the second Firefox is the recorder hangout
+    #RECORDER_SOURCE_OUTPUT=$(sed -n '2p' <<<"$FIREFOX_SOURCE_OUTPUT")
+    RECORDER_SINK_INPUT=$(sed -n '2p' <<<"$FIREFOX_SINK_INPUT")
+
+    # Assume the third Firefox is the countdown clock
+    CLOCK_SINK_INPUT=$(sed -n '3p' <<<"$FIREFOX_SINK_INPUT")
+
+    #pactl move-source-output $HOST_SOURCE_OUTPUT $HOST_SINK.monitor
+    pactl move-sink-input $HOST_SINK_INPUT $SINK
+    
+    #pactl move-source-output $RECORDER_SOURCE_OUTPUT $SILENCE_SINK.monitor
+    pactl move-sink-input $RECORDER_SINK_INPUT $RECORDER_SINK
+
+    pactl move-sink-input $CLOCK_SINK_INPUT $CLOCK_SINK
+
+    echo 'Firefox/Hangouts will not let us change the mic inputs'
+    echo 'Make the following changes manually:'
+    echo '- Host mic to "Monitor of LT Host"'
+    echo '- Recorder mic to "Monitor of LT Silence"'
+    echo
+    echo 'Firefox outputs may be mixed up too; if so:'
+    echo '- Host playback to "Voice WebRTC" or your headphones/speaker'
+    echo '- Recorder playback to "LT Recorder"'
+    echo '- Clock playback to "LT Clock"'
+}
+
+function find-app-source-output {
+    APP=$1
+    EXPECT=${2:-1}
+    RESULTS=$(pactl list source-outputs \
+        | sed -E ':begin;$!N;s/\n\s+/\; /;tbegin' \
+        | grep -E '^Source Output #[0-9]+.*?; application.name = "'$APP'"' \
+        | sed -E 's/^Source Output #([0-9]+).*$/\1/' \
+        || '')
+    COUNT=$(grep -c '' <<<"$RESULTS")
+    if [ "$RESULTS" == "" ]; then
+        COUNT=0
+    fi
+    if [ $COUNT != $EXPECT ]; then
+        echo "Could not find source output for $APP; expected $EXPECT, but found $COUNT" >&2
+        exit 1
+    fi
+    echo "$RESULTS"
+}
+
+function find-app-sink-input {
+    APP=$1
+    EXPECT=${2:-1}
+    RESULTS=$(pactl list sink-inputs \
+        | sed -E ':begin;$!N;s/\n\s+/\; /;tbegin' \
+        | grep -E '^Sink Input #[0-9]+.*?; application.name = "'$APP'"' \
+        | sed -E 's/^Sink Input #([0-9]+).*$/\1/' \
+        || '')
+    COUNT=$(grep -c '' <<<"$RESULTS")
+    if [ "$RESULTS" == "" ]; then
+        COUNT=0
+    fi
+    if [ $COUNT != $EXPECT ]; then
+        echo "Could not find sink input for $APP; expected $EXPECT, but found $COUNT" >&2
+        exit 1
+    fi
+    echo "$RESULTS"
+}
+
+ACTION=$1
+if [ "$ACTION" == "" ]; then
+    ACTION=setup
+fi
+
+case $ACTION in
+
+    setup)
+        setup
+        defaults
+        move-apps
+        ;;
+
+    cleanup)
+        cleanup
+        defaults
+        ;;
+
+    *)
+        echo "Unrecognized action: $ACTION" >&2
+        exit 1
+        ;;
+
+esac
--- src/pa-voice.sh
+++ src/pa-voice.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-SOURCE="${SOURCE:-`pacmd list-sources | grep -E -i '\* index:' | sed -E 's/^[^0-9]*([0-9]*).*$/\1/'`}"
-SINK="${SINK:-`pacmd list-sinks | grep -E -i '\* index:' | sed -E 's/^[^0-9]*([0-9]*).*$/\1/'`}"
+#SOURCE="${SOURCE:-`pacmd list-sources | grep -E -i '\* index:' | sed -E 's/^[^0-9]*([0-9]*).*$/\1/'`}"
+#SINK="${SINK:-`pacmd list-sinks | grep -E -i '\* index:' | sed -E 's/^[^0-9]*([0-9]*).*$/\1/'`}"
+
+HEADSET_MIC=alsa_input.usb-C-Media_Electronics_Inc._USB_Audio_Device-00.mono-fallback
+HEADSET_OUT=alsa_output.usb-C-Media_Electronics_Inc._USB_Audio_Device-00.analog-stereo
+SOURCE="${SOURCE:-$HEADSET_MIC}"
+SINK="${SINK:-$HEADSET_OUT}"
 
 WEBRTC_SINK=voice_webrtc
 WEBRTC_SOURCE=voice_webrtc_source
@@ -11,57 +16,96 @@ FINAL_SINK=voice_final
 FINAL_MONITOR=voice_final.monitor
 FINAL_SOURCE=voice_final_source
 
-pactl load-module module-echo-cancel \
-    use_master_format=1 \
-    source_master=$SOURCE \
-    sink_master=$SINK \
-    aec_method=webrtc \
-    aec_args="analog_gain_control=0\ digital_gain_control=1" \
-    source_name=$WEBRTC_SOURCE \
-    sink_name=$WEBRTC_SINK \
-    sink_properties='device.description="Voice\ WebRTC"' \
-    source_properties='device.description="Voice\ WebRTC\ Input"'
+function setup {
+    pactl load-module module-echo-cancel \
+        use_master_format=1 \
+        source_master=$SOURCE \
+        sink_master=$SINK \
+        aec_method=webrtc \
+        aec_args="analog_gain_control=0\ digital_gain_control=1" \
+        source_name=$WEBRTC_SOURCE \
+        sink_name=$WEBRTC_SINK \
+        sink_properties='device.description="Voice\ WebRTC\ Speaker"' \
+        source_properties='device.description="Voice\ WebRTC\ Mic"'
 
-pactl set-default-sink $WEBRTC_SINK
+    pactl set-default-sink $WEBRTC_SINK
 
-pactl load-module module-null-sink \
-    sink_name=$FINAL_SINK \
-    sink_properties='device.description="Voice\ Final"'
+    pactl load-module module-null-sink \
+        sink_name=$FINAL_SINK \
+        sink_properties='device.description="Voice\ Final"'
 
-# Gate control parameters:
-# 0. LF key filter (Hz) - Controls the cutoff of the low frequency filter (highpass).
-# 1. HF key filter (Hz) - Controls the cutoff of the high frequency filter (lowpass).
-# 2. Threshold (dB) - Controls the level at which the gate will open.
-# 3. Attack (ms) - Controls the time the gate will take to open fully.
-# 4. Hold (ms) - Controls the minimum time the gate will stay open for.
-# 5. Decay (ms) - Controls the time the gate will take to close fully.
-# 6. Range (dB) - Controls the difference between the gate's open and closed state.
-# 7. Output select (-1 = key listen, 0 = gate, 1 = bypass) - Controls output monitor. -1 is the output of the key filters (so you can check what is being gated on). 0 is the normal, gated output. 1 is bypass mode. 
-pactl load-module module-ladspa-sink \
-    sink_name=$GATE_SINK \
-    sink_master=$FINAL_SINK \
-    plugin=gate_1410 \
-    label=gate \
-    control=500,4000,-42,25,75,250,-90,0 \
-    sink_properties='device.description="Voice\ Gate"'
+    # Gate control parameters:
+    # 0. LF key filter (Hz) - Controls the cutoff of the low frequency filter (highpass).
+    # 1. HF key filter (Hz) - Controls the cutoff of the high frequency filter (lowpass).
+    # 2. Threshold (dB) - Controls the level at which the gate will open.
+    # 3. Attack (ms) - Controls the time the gate will take to open fully.
+    # 4. Hold (ms) - Controls the minimum time the gate will stay open for.
+    # 5. Decay (ms) - Controls the time the gate will take to close fully.
+    # 6. Range (dB) - Controls the difference between the gate's open and closed state.
+    # 7. Output select (-1 = key listen, 0 = gate, 1 = bypass) - Controls output monitor. -1 is the output of the key filters (so you can check what is being gated on). 0 is the normal, gated output. 1 is bypass mode. 
+    pactl load-module module-ladspa-sink \
+        sink_name=$GATE_SINK \
+        sink_master=$FINAL_SINK \
+        plugin=gate_1410 \
+        label=gate \
+        control=500,4000,-42,25,75,250,-90,0 \
+        sink_properties='device.description="Voice\ Gate"'
 
-pactl load-module module-loopback \
-    source=$WEBRTC_SOURCE \
-    sink=$GATE_SINK \
-    source_dont_move=true \
-    sink_dont_move=true
-
-pactl load-module module-virtual-source \
-    source_name=$FINAL_SOURCE \
-    source_properties='device.description="Voice\ Final\ Input"' \
-    master=$FINAL_MONITOR
-
-pactl set-default-source $FINAL_SOURCE
-
-if [ "$MONITOR" == "1" ]; then
     pactl load-module module-loopback \
-        source=$FINAL_SOURCE \
-        sink=$WEBRTC_SINK \
+        source=$WEBRTC_SOURCE \
+        sink=$GATE_SINK \
         source_dont_move=true \
-        sink_dont_move=true
+        sink_dont_move=true \
+        latency_msec=1
+
+    pactl load-module module-virtual-source \
+        source_name=$FINAL_SOURCE \
+        source_properties='device.description="Voice\ Final\ Mic"' \
+        master=$FINAL_MONITOR
+
+    pactl set-default-source $FINAL_SOURCE
+
+    if [ "$MONITOR" == "1" ]; then
+        pactl load-module module-loopback \
+            source=$FINAL_SOURCE \
+            sink=$SINK \
+            source_dont_move=true \
+            sink_dont_move=true \
+            latency_msec=1
+    fi
+}
+
+function cleanup {
+    MODULES=$(pactl list short modules | grep -E '^[0-9]+\s+module-(echo-cancel|(null-sink|ladspa-sink|virtual-source)\s+(sink|source)_name=voice_)' | sed -E 's/^([0-9]+).*$/\1/')
+
+    while read MODULE; do
+        if [ "$MODULE" != "" ]; then
+            pactl unload-module $MODULE
+        fi
+    done <<<"$MODULES"
+
+    #pactl set-default-source $SOURCE
+    #pactl set-default-sink $SINK
+}
+
+ACTION=$1
+if [ "$ACTION" == "" ]; then
+    ACTION=setup
 fi
+
+case $ACTION in
+
+    setup)
+        setup
+        ;;
+
+    cleanup)
+        cleanup
+        ;;
+
+    *)
+        echo "Unrecognized action: $ACTION" >&2
+        exit 1
+        ;;
+
+esac
